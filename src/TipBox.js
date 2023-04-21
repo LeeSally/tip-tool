43344dc4f33ccd56157381c5d0a6532ac785ba17
Lee.Sally <1753844220@qq.com>
2023-04-21 17:29:48 +0800

    fix: code formating

--- src/TipBox.js
+++ src/TipBox.js
@@ -1,58 +1,58 @@
-import React from 'react'; 
+import React from 'react';
 import './TipBox.css';
 
-class ToolTip extends React.Component{ 
-    render(){ 
-        return(  
-            <div style={{opacity:this.props.show?"0.8":"0"}}
-             className= {"tool-tip-box tool-tip-box-" + this.props.type} >
-                {this.props.value}
-            </div>
-        );
-    }
+class ToolTip extends React.Component {
+  render() {
+    return (
+      <div style={{ opacity: this.props.show ? "0.8" : "0" }}
+        className={"tool-tip-box tool-tip-box-" + this.props.type} >
+        {this.props.value}
+      </div>
+    );
+  }
 }
 
-export default class TipBox extends React.Component{
-    constructor(){
-        super(); 
-        this.state={
-            show:false
-        }
+export default class TipBox extends React.Component {
+  constructor() {
+    super();
+    this.state = {
+      show: false
     }
+  }
 
-    handlClick=()=>{
-        if(this.props.showEvent==='click'){
-            this.setState(
-                {show:!this.state.show}
-            ); 
-        } 
+  handlClick = () => {
+    if (this.props.showEvent === 'click') {
+      this.setState(
+        { show: !this.state.show }
+      );
     }
+  }
 
-    handlMouseOver=()=>{
-        if(this.props.showEvent==='hover'){
-            this.setState(
-                {show:true}
-            ); 
-        } 
+  handlMouseOver = () => {
+    if (this.props.showEvent === 'hover') {
+      this.setState(
+        { show: true }
+      );
     }
+  }
 
-    handlMouseOut=()=>{
-        if(this.props.showEvent==='hover'){
-            this.setState(
-                {show:false}
-            ); 
-        } 
+  handlMouseOut = () => {
+    if (this.props.showEvent === 'hover') {
+      this.setState(
+        { show: false }
+      );
     }
+  }
 
-    render(){ 
-        return(
-            <div className="box" onClick={this.handlClick} 
-                     onMouseOver ={this.handlMouseOver} onMouseOut={this.handlMouseOut}>
-                <div >
-                    {this.props.title}
-                </div>
-                <ToolTip value={this.props.value} type={this.props.tiptype} show={this.state.show}/>
-            </div>
-        );
-    }
+  render() {
+    return (
+      <div className="box" onClick={this.handlClick}
+        onMouseOver={this.handlMouseOver} onMouseOut={this.handlMouseOut}>
+        <div >
+          {this.props.title}
+        </div>
+        <ToolTip value={this.props.value} type={this.props.tiptype} show={this.state.show} />
+      </div>
+    );
+  }
 }
